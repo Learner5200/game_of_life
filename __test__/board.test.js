8e212048a6f08653f535303aa48578be41da0310
Chris Cooksley <christopher.cooksley@gmail.com>
2018-12-04 15:45:21 +0000

    Added board class with grid of cells

---
+++ __test__/board.test.js
@@ -0,0 +1,21 @@
+import Board from '../src/board';
+
+describe('Board', () => {
+  const CellMock = jest.fn();
+  const board = new Board({
+    width: 20,
+    height: 10,
+    CellClass: CellMock,
+  });
+  describe('.grid', () => {
+    it('has the height specified', () => {
+      expect(board.grid.length).toBe(10);
+    });
+    it('has the width specified', () => {
+      expect(board.grid[0].length).toBe(20);
+    });
+    it('contains cells of class specified', () => {
+      expect(board.grid[0][0]).toBeInstanceOf(CellMock);
+    });
+  });
+});
---
+++ src/board.js
@@ -0,0 +1,23 @@
+export default class Board {
+  constructor({
+    width, height, CellClass,
+  }) {
+    this.width = width;
+    this.height = height;
+    this.CellClass = CellClass;
+    this.grid = this._createGrid();
+  }
+
+  _createGrid() {
+    const grid = [];
+    for (let i = 0; i < this.height; i += 1) {
+      grid.push([]);
+    }
+    grid.forEach((row) => {
+      for (let i = 0; i < this.width; i += 1) {
+        row.push(new this.CellClass());
+      }
+    });
+    return grid;
+  }
+}
