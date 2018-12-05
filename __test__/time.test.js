e2fdb9719eb5b3bcef16c8568ed4e8d56bcb204c
Chris Cooksley <christopher.cooksley@gmail.com>
2018-12-05 09:54:26 +0000

    Time.tick() kills correct cells

---
+++ __test__/time.test.js
@@ -0,0 +1,56 @@
+import Time from '../src/time';
+
+describe('Time', () => {
+  let cell;
+  let boardMock;
+
+  describe('#tick', () => {
+    beforeEach(() => {
+      cell = {
+        livingNeighbours: () => [],
+        live: () => 'alive',
+        die: () => 'dead',
+      };
+      boardMock = {
+        cells: () => [cell],
+      };
+    });
+
+    describe('the tick giveth', () => {
+      it('resurrects cells with exactly 3 living neighbours', () => {
+
+      });
+    });
+
+    describe('the tick taketh away', () => {
+      it('kills cells with fewer than 2 living neighbours', () => {
+        cell.livingNeighbours = () => ['Neighbour1'];
+        const spy = jest.spyOn(cell, 'die');
+        Time.tick(boardMock);
+        expect(spy).toHaveBeenCalled();
+        spy.mockRestore();
+      });
+      it('kills cells with more than 3 living neighbours', () => {
+        cell.livingNeighbours = () => ['Neighbour1', 'Neighbour2', 'Neighbour3', 'Neighbour4'];
+        const spy = jest.spyOn(cell, 'die');
+        Time.tick(boardMock);
+        expect(spy).toHaveBeenCalled();
+        spy.mockRestore();
+      });
+      it('does not kill cells with 2 living neighbours', () => {
+        cell.livingNeighbours = () => ['Neighbour1', 'Neighbour2'];
+        const spy = jest.spyOn(cell, 'die');
+        Time.tick(boardMock);
+        expect(spy).not.toHaveBeenCalled();
+        spy.mockRestore();
+      });
+      it('does not kill cells with 3 living neighbours', () => {
+        cell.livingNeighbours = () => ['Neighbour1', 'Neighbour2', 'Neighbour3'];
+        const spy = jest.spyOn(cell, 'die');
+        Time.tick(boardMock);
+        expect(spy).not.toHaveBeenCalled();
+        spy.mockRestore();
+      });
+    });
+  });
+});
---
+++ src/time.js
@@ -0,0 +1,13 @@
+export default class Time {
+  static tick(board) {
+    board.cells().forEach((cell) => {
+      this._decideFate(cell);
+    });
+  }
+
+  static _decideFate(cell) {
+    const neighbours = cell.livingNeighbours().length;
+    if (neighbours > 3 || neighbours < 2) cell.die();
+    // if (neighbours === 3) cell.live();
+  }
+}
