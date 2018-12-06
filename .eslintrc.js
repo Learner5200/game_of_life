e477bdaa6eb9de134442073d4b241149810aaa6d
Chris Cooksley <christopher.cooksley@gmail.com>
2018-12-06 11:39:23 +0000

    removed prefer destructuring for arrays

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     "extends": "airbnb",
     "env": { "jest": true },
     "rules": {
-      "no-underscore-dangle": 0
+      "no-underscore-dangle": 0,
+      "prefer-destructuring": ["error", {"object": true, "array": false}]
     }
 };
